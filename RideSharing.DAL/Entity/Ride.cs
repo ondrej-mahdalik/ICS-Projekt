253e58da243d5ec68d9333cb66e28f0c4c3f1c7b
Ondřej Mahdalík <ondrej.mahdalik@gmail.com>
2022-03-03 19:48:04 +0100

    Základní entity

---
+++ RideSharing.DAL/Entity/Ride.cs
@@ -0,0 +1,27 @@
+﻿namespace RideSharing.DAL.Entity
+{
+    public class Ride
+    {
+        public Guid RideId { get; set; }
+        public Location From { get; init; }
+        public Location To { get; init; }
+        public DateTime Departure { get; set; }
+        public DateTime Arrival { get; set; }
+
+        public Guid DriverId { get; set; }
+        public User Driver { get; set; }
+
+        public ICollection<User> Passengers { get; set; }
+
+        public Review Review { get; set; }
+        public string Note { get; set; }
+
+    }
+
+    public struct Location
+    {
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ RideSharing.DAL/Entity/User.cs
@@ -0,0 +1,12 @@
+﻿namespace RideSharing.DAL.Entity
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        ICollection<Review> Reviews { get; set; }
+        ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
