ee1b87e8491bed89869b9fff129afcba1924f87f
Ondřej Mahdalík <ondrej.mahdalik@gmail.com>
2022-03-12 09:21:40 +0100

    Přidání chybějící reference + (snad) oprava formátování

--- src/RideSharing.DAL/Entities/VehicleEntity.cs
+++ src/RideSharing.DAL/Entities/VehicleEntity.cs
@@ -2,8 +2,12 @@
 
 namespace RideSharing.DAL.Entities
 {
-    public record VehicleEntity(Guid Id, VehicleType Type, string Make, string Model, DateTime Registered, ushort Seats,
-        string? ImageUrl) : IEntity;
+    public record VehicleEntity(Guid Id, Guid OwnerId, VehicleType Type, string Make, string Model, DateTime Registered,
+        ushort Seats,
+        string? ImageUrl) : IEntity
+    {
+        public UserEntity? Owner { get; init; }
+    }
 
     public enum VehicleType
     {
