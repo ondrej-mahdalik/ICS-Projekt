83f06aeaeb405eef96e40e2f2802b94b64fb8e5e
Aram Denk <aramde@seznam.cz>
2022-03-17 22:08:27 +0100

    Seedy

---
+++ src/RideSharing.DAL/Seeds/RideSeeds.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using RideSharing.DAL.Entities;
+
+namespace RideSharing.DAL.Seeds;
+
+public static class RideSeeds
+{
+    public static readonly RideEntity PrahaBrno = new(
+        Id: Guid.Parse("42b612c1-b668-4168-9b73-71acfb64f094"),
+        FromName: "Praha",
+        FromLatitude: 50.07698467371664,
+        FromLongitude: 14.432483187893586,
+        ToName: "Brno",
+        ToLatitude: 49.22611604448722,
+        ToLongitude: 16.582455843955017,
+        Departure: DateTime.Parse("22.2.2022 15:00"),
+        Arrival: DateTime.Parse("22.2.2022 18:00"),
+        DriverId: Guid.Parse(input: "f34cd643-1226-406d-971d-b5e6f745938e"),
+        VehicleId: Guid.Parse(input: "0a5ef2a1-d541-45ac-82a3-6b63a24d0572"),
+        Note: "No eating in the car !"
+    );
+
+    static RideSeeds()
+    {
+        PrahaBrno.Reservations.Add(ReservationSeeds.BrnoTwoSeats);
+        PrahaBrno.Reviews.Add(ReviewSeeds.Perfect);
+    }
+
+    public static void Seed(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<RideEntity>().HasData(
+            PrahaBrno
+        );
+    }
+}
---
+++ src/RideSharing.DAL/Seeds/UserSeeds.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using RideSharing.DAL.Entities;
+
+namespace RideSharing.DAL.Seeds;
+
+public static class UserSeeds
+{
+    public static readonly UserEntity JohnDoe = new(
+        Id: Guid.Parse(input: "f34cd643-1226-406d-971d-b5e6f745938e"),
+        Name: "John",
+        Surname: "Doe",
+        Phone: "737195090",
+        ImageUrl: "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500");
+
+    public static readonly UserEntity ElonTusk = new(
+        Id: Guid.Parse(input: "505b1e64-ed3c-44d1-883e-67de32b3ca59"),
+        Name: "Elon",
+        Surname: "Tusk",
+        Phone: "585453123",
+        ImageUrl: "https://pbs.twimg.com/profile_images/1443129819122782209/sqba2I3D_400x400.jpg"
+    );
+    static UserSeeds()
+    {
+        JohnDoe.Vehicles.Add(VehicleSeeds.Felicia);
+        JohnDoe.Reviews.Add(ReviewSeeds.Perfect);
+        ElonTusk.Reservations.Add(ReservationSeeds.BrnoTwoSeats);
+    }
+
+    public static void Seed(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserEntity>().HasData(
+            JohnDoe, ElonTusk
+        );
+    }
+}
