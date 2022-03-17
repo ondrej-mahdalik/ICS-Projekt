a42ef7c2275ce74082b6555db0a1efa771a60b29
Aram Denk <aramde@seznam.cz>
2022-03-17 22:08:41 +0100

    Factories

---
+++ src/RideSharing.DAL/Factories/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace RideSharing.DAL.Factories
+{
+    /// <summary>
+    /// EF Core CLI migration generation uses this DbContext to create model and migration
+    /// </summary>
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<RideSharingDbContext>
+    {
+        public RideSharingDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<RideSharingDbContext> builder = new();
+            builder.UseSqlServer(
+                @"Data Source=(LocalDB)\MSSQLLocalDB;
+                Initial Catalog = RideSharing;
+                MultipleActiveResultSets = True;
+                Integrated Security = True; ");
+
+            return new RideSharingDbContext(builder.Options);
+        }
+    }
+}
