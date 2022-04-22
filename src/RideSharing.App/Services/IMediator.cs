1e624f2fb130f0618df1e78b66e894fb560cd74f
Marko Kubrachenko <markubrachenko@gmail.com>
2022-04-22 22:12:14 +0200

    pridani slozek Commands, Services, Converters pro WPF

---
+++ src/RideSharing.App/Services/IMediator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RideSharing.App.Messages;
+
+namespace RideSharing.App.Services;
+
+public interface IMediator
+{
+    void Register<TMessage>(Action<TMessage> action)
+        where TMessage : IMessage;
+    void Send<TMessage>(TMessage message)
+        where TMessage : IMessage;
+    void UnRegister<TMessage>(Action<TMessage> action) 
+        where TMessage : IMessage;
+}
