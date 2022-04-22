65fbdc10f2763eb4da373d086ece8687c24f99e0
honzav23 <janvaclavik@valachnet.cz>
2022-04-22 21:13:39 +0200

    Kostra VehicleListViewModelu

---
+++ src/RideSharing.App/ViewModels/VehicleListViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using RideSharing.App.ViewModels.Interfaces;
+using RideSharing.BL.Facades;
+using RideSharing.BL.Models;
+using RideSharing.App.Extensions;
+
+namespace RideSharing.App.ViewModels
+{
+    public class VehicleListViewModel : ViewModelBase // TODO add Interface
+    {
+        private readonly VehicleFacade _vehicleFacade;
+        // TODO Add mediator and messageService
+
+
+        public VehicleListViewModel(
+            VehicleFacade vehicleFacade)
+        {
+            _vehicleFacade = vehicleFacade;
+            // TODO Add mediator and messageService to ctor
+            
+            // TODO assign commands
+
+            // TODO register mediator for creating, updating and deleting
+        }
+
+        public ICommand VehicleNewCommand { get; }
+        public ICommand VehicleDeleteCommand { get; }
+        public ICommand VehicleEditCommand { get; }
+
+        public ObservableCollection<VehicleListModel> Vehicles { get; set; } = new();
+
+        public async Task LoadAsync()
+        {
+            Vehicles.Clear();
+            var vehicles = await _vehicleFacade.GetAsync();
+            Vehicles.AddRange(vehicles);
+        }
+    }
+}
