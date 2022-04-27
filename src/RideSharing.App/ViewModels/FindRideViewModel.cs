6a6baf15cbe3cbe6a6ea5598489161c6e42373d5
Vilém Gottwald <xgottw07@vutbr.cz>
2022-04-27 23:13:47 +0200

    Nabindováno vyhledávání jízdy

--- src/RideSharing.App/ViewModels/FindRideViewModel.cs
+++ src/RideSharing.App/ViewModels/FindRideViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
+using RideSharing.App.Commands;
+using RideSharing.App.Messages;
 using RideSharing.App.Services;
 using RideSharing.App.ViewModels.Interfaces;
+using RideSharing.App.Wrappers;
 using RideSharing.BL.Facades;
+using RideSharing.BL.Models;
 
 namespace RideSharing.App.ViewModels;
 
@@ -15,7 +19,24 @@ public class FindRideViewModel : ViewModelBase, IFindRideViewModel
     {
         _rideFacade = rideFacade;
         _mediator = mediator;
+
+       // ReviewSubmittedCommand = new RelayCommand(ReviewSubmitted);
+        //RideDetailClickedCommand = new RelayCommand<RideListModel>(RideDetailClicked);
+
+        mediator.Register<UpdateMessage<RideWrapper>>(FindRide);
     }
+
+    public string SelFrom { get; set; } = "";
+    public string SelTo { get; set; } = "";
+    public DateTime? SelDate { get; set; } = null;
+    public TimeSpan? SelTime { get; set; } = null;
+    public bool SelDeparture { get; set; } = true;
+
+
+
+
+    private async void FindRide(UpdateMessage<RideWrapper> _) => await LoadAsync();
+
 
 #pragma warning disable CS1998 // V této asynchronní metodě chybí operátory await a spustí se synchronně.
     public async Task LoadAsync()
