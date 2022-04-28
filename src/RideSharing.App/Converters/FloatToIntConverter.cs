566d803bbafd601c5e3699f85c335c04ede6d279
Ondřej Mahdalík <ondrej.mahdalik@gmail.com>
2022-04-28 17:57:25 +0200

    Oprava času odjezdu, drobné úpravy stylu

---
+++ src/RideSharing.App/Converters/FloatToIntConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RideSharing.App.Converters;
+
+public class FloatToIntConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null || value.Equals(float.NaN))
+            return 0;
+
+        return (int)(float)value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
