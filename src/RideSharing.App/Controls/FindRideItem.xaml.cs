438ce0906b5da6caf1f286ca08eecfe04675f0e8
Ondřej Mahdalík <ondrej.mahdalik@gmail.com>
2022-04-28 14:27:04 +0200

    Přesunutí části kódu do controls

--- src/RideSharing.App/Controls/FindRideItem.xaml.cs
+++ src/RideSharing.App/Controls/FindRideItem.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.CodeDom;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 using RideSharing.Common.Enums;
 
---
+++ src/RideSharing.App/Controls/FromToBlock.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+
+namespace RideSharing.App.Controls
+{
+    /// <summary>
+    /// Interakční logika pro FromToBlock.xaml
+    /// </summary>
+    public partial class FromToBlock
+    {
+        public string FromName
+        {
+            get => (string)GetValue(FromNameProperty);
+            set => SetValue(FromNameProperty, value);
+        }
+
+        public string ToName
+        {
+            get => (string)GetValue(ToNameProperty);
+            set => SetValue(ToNameProperty, value);
+        }
+
+        public DateTime Departure
+        {
+            get => (DateTime)GetValue(DepartureProperty);
+            set => SetValue(DepartureProperty, value);
+        }
+
+        public DateTime Arrival
+        {
+            get => (DateTime)GetValue(ArrivalProperty);
+            set => SetValue(ArrivalProperty, value);
+        }
+
+        public static readonly DependencyProperty FromNameProperty = DependencyProperty.Register(
+            nameof(FromName), typeof(string), typeof(FromToBlock), new PropertyMetadata(default));
+
+        public static readonly DependencyProperty ToNameProperty = DependencyProperty.Register(
+            nameof(ToName), typeof(string), typeof(FromToBlock), new PropertyMetadata(default));
+
+        public static readonly DependencyProperty DepartureProperty = DependencyProperty.Register(
+            nameof(Departure), typeof(DateTime), typeof(FromToBlock), new PropertyMetadata(default));
+
+        public static readonly DependencyProperty ArrivalProperty = DependencyProperty.Register(
+            nameof(Arrival), typeof(DateTime), typeof(FromToBlock), new PropertyMetadata(default));
+
+        public FromToBlock()
+        {
+            InitializeComponent();
+        }
+    }
+}
