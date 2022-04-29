a1e796b6a564c4ed104d2d76ae81967cbea8851d
Marko Kubrachenko <markubrachenko@gmail.com>
2022-04-29 16:06:22 +0200

    Pridani testu do ReservationFacadeTests

--- src/RideSharing.BL.Tests/ReservationFacadeTests.cs
+++ src/RideSharing.BL.Tests/ReservationFacadeTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using RideSharing.BL.Facades;
@@ -33,8 +34,63 @@ public sealed class ReservationFacadeTests : CRUDFacadeTestsBase
 
         // Assert
         await using var dbxAssert = await DbContextFactory.CreateDbContextAsync();
-       var temp = await dbxAssert.ReservationEntities.Include(x => x.ReservingUser).Include(x => x.Ride).FirstOrDefaultAsync(x =>
+        var temp = await dbxAssert.ReservationEntities.Include(x => x.ReservingUser).Include(x => x.Ride).FirstOrDefaultAsync(x => 
             x.ReservingUserId == UserSeeds.JustSubmittedReviewUser.Id && x.RideId == RideSeeds.JustReviewRide.Id);
-       Assert.NotNull(temp);
+        Assert.NotNull(temp);
+    }
+    
+    [Fact]
+    public async Task UpdateReservation()
+    {
+        // Arrange
+        ReservationDetailModel reservation = new(DateTime.Now, 3)
+        {
+            ReservingUser = Mapper.Map<UserDetailModel>(UserSeeds.ReservationUser1),
+            Ride = Mapper.Map<RideDetailModel>(RideSeeds.JustReviewRide)
+        };
+        
+        reservation.ReservingUser = Mapper.Map<UserDetailModel>(UserSeeds.ReservationUser2);
+
+        // Act
+        await _reservationFacadeSUT.SaveAsync(reservation);
+
+        // Assert
+        await using var dbxAssert = await DbContextFactory.CreateDbContextAsync();
+        var temp = await dbxAssert.ReservationEntities.Include(x => x.ReservingUser).Include(x => x.Ride).FirstOrDefaultAsync(x => 
+            x.ReservingUserId == UserSeeds.ReservationUser2.Id && x.RideId == RideSeeds.JustReviewRide.Id);
+        Assert.NotNull(temp);
+    }
+    
+    [Fact]
+    public async Task GetAll_Single_SeededReservation()
+    {
+        var reservations = await _reservationFacadeSUT.GetAsync();
+        var reservation = reservations.Single(i => i.Id == ReservationSeeds.DriverBrnoBratislava.Id);
+        Assert.Equal(Mapper.Map<ReservationDetailModel>(ReservationSeeds.DriverBrnoBratislava).Id, reservation.Id);
+        Assert.Equal(Mapper.Map<UserDetailModel>(UserSeeds.DriverUser).Id, reservation.ReservingUser!.Id);
+        Assert.Equal(Mapper.Map<RideDetailModel>(RideSeeds.BrnoBratislava).Id, reservation.Ride!.Id);
+    }
+    
+    [Fact]
+    public async Task GetById_SeededReservation()
+    {
+        var reservation = await _reservationFacadeSUT.GetAsync(ReservationSeeds.User1BrnoBratislava.Id);
+        Assert.NotNull(reservation);
+        Assert.Equal(Mapper.Map<ReservationDetailModel>(ReservationSeeds.User1BrnoBratislava).Id, reservation!.Id);
+    }
+    
+    [Fact]
+    public async Task SeededReservation_DeleteByIdDeleted()
+    {
+        await _reservationFacadeSUT.DeleteAsync(ReservationSeeds.DriverBrnoBratislava.Id);
+        await using var dbxAssert = await DbContextFactory.CreateDbContextAsync();
+        Assert.False(await dbxAssert.ReservationEntities.AnyAsync(i => i.Id == ReservationSeeds.DriverBrnoBratislava.Id));
+    }
+    
+    [Fact]
+    public async Task GetById_NonExistentReservation()
+    {
+        var reservation = await _reservationFacadeSUT.GetAsync(Guid.Parse("D2453E4A-2A52-4199-A8BE-254853C575B6")); // Guid for non-existing seed
+        Assert.Null(reservation);
     }
 }
