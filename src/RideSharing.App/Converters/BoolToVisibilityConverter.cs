eb0b764f8bdb6d3390835317a56bfa239025c422
Ondřej Mahdalík <ondrej.mahdalik@gmail.com>
2022-05-02 14:51:31 +0200

    Hodnocení jízd

---
+++ src/RideSharing.App/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RideSharing.App.Converters;
+
+public class BoolToVisibilityConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (parameter is not null && parameter.ToString() ==  "true") // Hidden when true
+        {
+            if (value is null)
+                return Visibility.Visible;
+
+            return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+        }
+        else // Visible when false
+        {
+            if (value is null)
+                return Visibility.Collapsed;
+
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/RideSharing.App/Views/DashboardView.xaml.cs
+++ src/RideSharing.App/Views/DashboardView.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace RideSharing.App.Views
+﻿using System.Windows;
+using MaterialDesignThemes.Wpf;
+using RideSharing.App.ViewModels;
+using RideSharing.BL.Models;
+
+namespace RideSharing.App.Views
 {
     /// <summary>
     /// Interakční logika pro DashboardView.xaml
@@ -8,6 +13,14 @@
         public DashboardView()
         {
             InitializeComponent();
+        }
+
+        private void RatingBar_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+            var ride = (RideRecentListModel)((RatingBar)sender).Tag;
+            var viewModel = (DashboardViewModel)DataContext;
+            if (viewModel.ReviewSubmittedCommand.CanExecute(ride))
+                viewModel.ReviewSubmittedCommand.Execute(ride);
         }
     }
 }
