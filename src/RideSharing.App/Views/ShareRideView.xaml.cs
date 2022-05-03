f10e853b8fcc407bdd501aa37978ae86e7a7913b
Ondřej Mahdalík <ondrej.mahdalik@gmail.com>
2022-05-03 11:32:55 +0200

    View pro vytvoření jízdy

---
+++ src/RideSharing.App/Views/ShareRideView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace RideSharing.App.Views
+{
+    /// <summary>
+    /// Interakční logika pro ShareRideView.xaml
+    /// </summary>
+    public partial class ShareRideView
+    {
+        public ShareRideView()
+        {
+            InitializeComponent();
+        }
+    }
+}
